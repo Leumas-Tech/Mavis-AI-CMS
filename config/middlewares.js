86c61f9375ff3fa162e7239298ffb0892b61de1e
william7829 <92896220+william7829@users.noreply.github.com>
2024-04-15 13:27:16 -0500

    Fixing cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,32 +8,28 @@ module.exports = [
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  {
-    // Ensure the public middleware is correctly configured to serve static files
-    name: 'strapi::public',
-    config: {
-      maxAge: 60000, // Example: Cache static assets for 60 seconds for browsers
-    },
-  },
+  'strapi::public',
+
   {
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:', 'wss:'], // Ensure WebSocket and secure origins are allowed
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com', 'yourserver.com'], // Ensure images are loaded correctly
-          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com', 'yourserver.com'], // Include your server domain if media files are hosted there
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  {
-    // CORS settings: adjust according to your frontend's domain to allow requests
-    name: 'strapi::cors',
-    config: {
-      origin: ["http://localhost:3000", "https://yourfrontenddomain.com"], // Add your frontend domain here
-    },
-  },
+  
+  
 ];
